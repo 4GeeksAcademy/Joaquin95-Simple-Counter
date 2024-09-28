7eb808676daa52c270611a82b83ee7dbc4495b3d
Joaquin Morales <Mintinvestments95@gmail.com>
2024-09-28 01:15:51 +0000

    Not completed

--- src/js/component/SecondsCounter.jsx
+++ src/js/component/SecondsCounter.jsx
@@ -2,10 +2,24 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const SecondsCounter = ({ seconds }) => {
+
+  const thousands = Math.floor(seconds / 1000) % 10;
+  const months = Math.floor(seconds / (3600 * 24 * 30));
+  const days = Math.floor((seconds % 3600 *24 * 30) / (3600 * 24));
+  const hours = Math.floor((seconds % (3600 * 24)) / 3600);
+  const minutes = Math.floor((seconds / 3600 * 24 ) / 3600);
+  const secs = seconds % 60;
+
   return (
-    <div className="counter-container text-center">
-      <i className="fas fa-clock"></i>
-      <div className="d-inline display-4 mx-3">{seconds}</div>
+    <div className="counter-container text-center d-flex justify-content-center align-items-center border border-dark p-3 rounded">
+      {" "}
+      <i className="fa fa-clock display-3 mx-2 digit-box"></i>
+      <div className="digit-box">{thousands}</div>
+      <div className="digit-box">{months}</div>
+      <div className="digit-box">{days}</div>
+      <div className="digit-box">{hours}</div>
+      <div className="digit-box">{minutes}</div>
+      <div className="digit-box">{secs}</div>
     </div>
   );
 };
@@ -14,4 +28,4 @@ SecondsCounter.propTypes = {
   seconds: PropTypes.number.isRequired,
 };
 
-export default SecondsCounter; // Make sure to export the component
+export default SecondsCounter;
