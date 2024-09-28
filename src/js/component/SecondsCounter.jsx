534e4a73c511388c4df21550a5f1fc70cccb0dbf
Joaquin Morales <Mintinvestments95@gmail.com>
2024-09-28 12:54:37 +0000

    Completed

--- src/js/component/SecondsCounter.jsx
+++ src/js/component/SecondsCounter.jsx
@@ -3,23 +3,21 @@ import PropTypes from "prop-types";
 
 const SecondsCounter = ({ seconds }) => {
 
-  const thousands = Math.floor(seconds / 1000) % 10;
-  const months = Math.floor(seconds / (3600 * 24 * 30));
-  const days = Math.floor((seconds % 3600 *24 * 30) / (3600 * 24));
-  const hours = Math.floor((seconds % (3600 * 24)) / 3600);
-  const minutes = Math.floor((seconds / 3600 * 24 ) / 3600);
-  const secs = seconds % 60;
+    const thousands = Math.floor(seconds / 1000) % 10;
+    const hundreds = Math.floor(seconds / 100) % 10;
+    const tens = Math.floor(seconds / 10) % 10;
+    const ones = seconds % 10;
 
   return (
     <div className="counter-container text-center d-flex justify-content-center align-items-center border border-dark p-3 rounded">
       {" "}
       <i className="fa fa-clock display-3 mx-2 digit-box"></i>
-      <div className="digit-box">{thousands}</div>
-      <div className="digit-box">{months}</div>
-      <div className="digit-box">{days}</div>
-      <div className="digit-box">{hours}</div>
-      <div className="digit-box">{minutes}</div>
-      <div className="digit-box">{secs}</div>
+            <div className="digit-box bg-dark text-white mx-1 p-3 rounded">{0}</div>
+            <div className="digit-box bg-dark text-white mx-1 p-3 rounded">{0}</div>
+            <div className="digit-box bg-dark text-white mx-1 p-3 rounded">{thousands}</div>
+            <div className="digit-box bg-dark text-white mx-1 p-3 rounded">{hundreds}</div>
+            <div className="digit-box bg-dark text-white mx-1 p-3 rounded">{tens}</div>
+            <div className="digit-box bg-dark text-white mx-1 p-3 rounded">{ones}</div>
     </div>
   );
 };
